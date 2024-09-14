0e8553ffe4ab3a505b051e1c678dfda068bfff66
saitim <santinocirico@gmail.com>
2024-09-14 15:56:05 -0300

    SCRUM 35 y 77 add the player and the game class

---
+++ src/game.py
@@ -0,0 +1,8 @@
+"""Module providing the game class"""
+
+class game:
+    """Class representing a game"""
+    def __init__(self, gameid, name, max_players):
+        game.gameid = gameid
+        game.name = name
+        game.max_players = max_players
---
+++ src/player.py
@@ -0,0 +1,7 @@
+"""Module providing the player class"""
+
+class Player:
+    """Class representing a player in the game"""
+    def __init__(self, playerid, name):
+        Player.playerid = playerid
+        Player.name = name
