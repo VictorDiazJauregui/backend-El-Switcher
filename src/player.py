8b2de0cebf2b42d678d60260f3ac31a0fc7ca9b9
Facundo Alvarez Miranda <facualvarezmiranda@hotmail.com>
2024-09-16 12:40:15 -0300

    project structure and changes to models

--- src/player.py
+++
@@ -1,8 +0,0 @@
-"""Module providing the player class"""
-from pydantic import BaseModel
-
-class Player(BaseModel):
-    """Class representing a player in the game"""
-    id: int
-    name: str
-    
