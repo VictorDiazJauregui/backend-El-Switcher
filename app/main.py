47e8fa7a341081391e691587cad860a76f216b57
Agustin Farace <agustin.farace@mi.unc.edu.ar>
2024-09-20 14:09:24 -0300

    SCRUM-39 add leave game router

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from contextlib import asynccontextmanager
 from app.db.db import Base, engine
 from pydantic import ValidationError
 
-from app.routers import game, list, join, start
+from app.routers import game, list, join, start, leave
 from app.errors.handlers import value_error_handler, generic_exception_handler, validation_exception_handler
 
 @asynccontextmanager
@@ -53,6 +53,7 @@ app.include_router(game.router)
 app.include_router(list.router)
 app.include_router(join.router)
 app.include_router(start.router)
+app.include_router(leave.router)
 
 @app.get("/")
 def read_root():
