1187bc636d69a3a368876af3d2f1dd29a4a597bd
Heber Alturria <heber.alturria.git@gmail.com>
2024-09-29 23:12:57 -0300

    SCRUM-91 Add example of lobby socket

--- app/routers/sio_game.py
+++ app/routers/sio_game.py
@@ -15,14 +15,17 @@ async def connect(sid, environ, auth):
 
     print(f'Player {player_id} connected to game {game_id}')
     
+    # Example to use the database
     with db_context() as db:
         game = db.query(Game).filter(Game.id == game_id).first()
+
         if game is None:
             print(f'Game {game_id} does not exist')
             return # Game does not exist, then disconnect the player
         
         # check if the player is part of the game
         player = db.query(Player).filter_by(id=player_id, game_id=game.id).first()
+
         if player is None:
             print(f'Player {player_id} is not part of game {game_id}')
             return # Player is not part of the game, then disconnect the player
@@ -32,4 +35,5 @@ async def connect(sid, environ, auth):
         
         await broadcast.register_player_socket(sio_game, player_id, game_id, sid)
         
+        # Example to broadcast to all players in the game
         await broadcast.broadcast(sio_game, game_id, 'player_connected', {'playerId': player_id, 'playerName': player.name})
--- app/routers/sio_lobby.py
+++ app/routers/sio_lobby.py
@@ -1,4 +1,6 @@
+from app.db.db import db_context, Game, Player
 from app.utils.parse_query_string import parse_query_string
+from app.models.broadcast import Broadcast
 import socketio
 
 # Create a new Socket.IO server
@@ -13,3 +15,25 @@ async def connect(sid, environ, auth):
 
     print(f'Player {player_id} connected to lobby {game_id}')
 
+    # Example to use the database
+    with db_context() as db:
+        game = db.query(Game).filter(Game.id == game_id).first()
+
+        if game is None:
+            print(f'Game {game_id} does not exist')
+            return # Game does not exist, then disconnect the player
+        
+        # check if the player is part of the game
+        player = db.query(Player).filter_by(id=player_id, game_id=game.id).first()
+
+        if player is None:
+            print(f'Player {player_id} is not part of game {game_id}')
+            return # Player is not part of the game, then disconnect the player
+
+        # Register the player's socket
+        broadcast = Broadcast()
+        
+        await broadcast.register_player_socket(sio_lobby, player_id, game_id, sid)
+        
+        # Example to broadcast to all players in the game
+        await broadcast.broadcast(sio_lobby, game_id, 'start_game', {'canStart': False})
