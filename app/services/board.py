1f9f0e7efcdbc0c7166fa3a7c77bcf416d368900
Facundo Alvarez Miranda <facualvarezmiranda@hotmail.com>
2024-10-14 18:05:39 -0300

    fixes + test

--- app/services/board.py
+++ app/services/board.py
@@ -3,9 +3,9 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.schemas.board import PieceResponseSchema
 from app.schemas.move import MakeMoveSchema
 from app.db.db import Board, Color, SquarePiece, ParallelBoard, CardMove, Player
-from app.services.game_events import emit_board
 from typing import List
 import random
+import json
 
 def create_board(game_id: int, db: Session) -> List[PieceResponseSchema]:
     board = Board(game_id=game_id)
@@ -54,39 +54,40 @@ def get_board(game_id: int, db: Session) -> List[PieceResponseSchema]:
                 column=piece.column
             ).model_dump() for piece in square_pieces]
 
-def make_move(move_data: MakeMoveSchema, db: Session):
+async def make_move(move_data: MakeMoveSchema, db: Session):
     try:
         card_move = db.query(CardMove).filter(CardMove.id == move_data.movementCardId).first()
         if not card_move:
             raise ValueError("Invalid movementCardId")
-        
+
         player_id = card_move.owner_id
         player = db.query(Player).filter(Player.id == player_id).first()
         if not player:
             raise ValueError("Player not found")
         game_id = player.game_id
-        
+
         save_board(game_id, player_id, db)
-        switch_pieces(move_data.squarePieceId1, move_data.squarePieceId2, db)
-        emit_board(game_id, db)
+        switch_pieces(move_data.squarePieceId1, move_data.squarePieceId2, db)  
+        
+        from app.services.game_events import emit_board
+        await emit_board(game_id, db)
+        
     except SQLAlchemyError as e:
         db.rollback()
         raise RuntimeError(f"Error making move: {e}")
     except ValueError as e:
         raise RuntimeError(f"Validation error: {e}")
 
-    return 200
-
 def save_board(game_id: int, player_id: int, db: Session):
     try:
-        state_data = get_board(game_id, db)
-        
+        state_data = json.dumps(get_board(game_id, db))
+
         existing_states = db.query(ParallelBoard).filter_by(board_id=game_id).order_by(ParallelBoard.state_id).all()
         if existing_states:
             latest_state_id = (existing_states[-1].state_id % 3) + 1
         else:
             latest_state_id = 1
-        
+
         parallel_board = ParallelBoard(
             board_id=game_id,
             player_id=player_id,
