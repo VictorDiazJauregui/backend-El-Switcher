abeffd379193e5350f33111e570104dc3e8a9a4b
Facundo Alvarez Miranda <facualvarezmiranda@hotmail.com>
2024-11-01 20:55:16 -0300

    SCRUM-204 lo atamos con alambre no basa nada badre

--- app/services/cards.py
+++ app/services/cards.py
@@ -1,3 +1,4 @@
+import asyncio
 from random import shuffle
 
 from sqlalchemy.orm import Session
@@ -312,7 +313,7 @@ def fetch_figure_cards(game_id: int, db: Session):
         raise Exception(f"Error fetching cards: {e}")
 
 
-async def delete_figure_card(figureCardId: int, db: Session):
+def delete_figure_card(figureCardId: int, db: Session):
     """
     Deletes a figure card from the database.
     """
@@ -331,7 +332,7 @@ async def delete_figure_card(figureCardId: int, db: Session):
     except SQLAlchemyError as e:
         raise Exception(f"Error deleting figure card: {e}")
 
-    await win_by_figures(game_id, player_id, db)
+    asyncio.run(win_by_figures(game_id, player_id, db))
 
 
 async def win_by_figures(game_id: int, player_id: int, db: Session):
