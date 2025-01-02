116eb2fc9690b343cf9398eba4730c445ffba401
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2025-01-01 21:00:38 -0300

    Actualizado de los import db en app/services/logs.py

--- app/services/logs.py
+++ app/services/logs.py
@@ -1,7 +1,7 @@
-from app.db.db import LogMessage
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session, selectinload
 
+from app.db.models.log_message import LogMessage
 
 async def get_log_history(game_id: int, db: Session):
     result = db.execute(
