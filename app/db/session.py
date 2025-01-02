541d37341739f9b104ca7cd58721f730bbc2ebd9
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2025-01-01 21:26:07 -0300

    Formateado del código en app/db/session.py

--- app/db/session.py
+++ app/db/session.py
@@ -6,11 +6,13 @@ DATABASE_URL = "mysql+pymysql://root:secret@localhost:33061/switcher"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
 
 db_context = contextmanager(get_db)
