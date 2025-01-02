f06295c934cd0fa74793561fbc6521a04c7a2bf5
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2025-01-01 21:26:07 -0300

    Formateado del código en app/db/models/parallel_board.py

--- app/db/models/parallel_board.py
+++ app/db/models/parallel_board.py
@@ -2,16 +2,25 @@ from sqlalchemy import Column, Integer, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from app.db.base import Base
 
+
 class ParallelBoard(Base):
     __tablename__ = "parallel_boards"
 
-    id = Column(Integer, primary_key=True)  # This is the parallelboard unique id
-    board_id = Column(Integer, ForeignKey("boards.game_id"))  # Same id as board and game
-    player_id = Column(Integer, ForeignKey("players.id"))  # Player who made the move
+    id = Column(
+        Integer, primary_key=True
+    )  # This is the parallelboard unique id
+    board_id = Column(
+        Integer, ForeignKey("boards.game_id")
+    )  # Same id as board and game
+    player_id = Column(
+        Integer, ForeignKey("players.id")
+    )  # Player who made the move
     state_id = Column(Integer, nullable=False)  # This is the state_id, abrazo.
     # state_id = 1-3, 1 = Inicial, 2 = Primer movimiento, 3 = Segundo movimiento
     state_data = Column(Text, nullable=False)  # JSON string
-    move_asociated = Column(Integer, ForeignKey("card_moves.id"), nullable=True)
+    move_asociated = Column(
+        Integer, ForeignKey("card_moves.id"), nullable=True
+    )
 
     board = relationship("Board", back_populates="parallel_boards")
     player = relationship("Player", back_populates="parallel_boards")
