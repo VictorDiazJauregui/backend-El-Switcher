aba98eee7b8df4a5e9ec25a57907fdce643b6d77
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2025-01-01 21:26:07 -0300

    Formateado del código en app/services/logs.py

--- app/services/logs.py
+++ app/services/logs.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session, selectinload
 
 from app.db.models.log_message import LogMessage
 
+
 async def get_log_history(game_id: int, db: Session):
     result = db.execute(
         select(LogMessage)
