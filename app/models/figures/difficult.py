8d566f4675611f1e1d17a4d5437cf43a27a1a727
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2025-01-01 23:18:18 -0300

    Figuras difíciles modularizadas

---
+++ app/models/figures/difficult.py
@@ -0,0 +1,243 @@
+import numpy as np
+from .base import Figure
+
+
+class fig1(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 1",
+            np.array(
+                [
+                    ["*", "*", "*"],
+                    [None, "*", None],
+                    [None, "*", None],
+                ]
+            ),
+        )
+
+
+class fig2(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 2",
+            np.array(
+                [
+                    ["*", "*", None, None],
+                    [None, "*", "*", "*"],
+                ]
+            ),
+        )
+
+
+class fig3(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 3",
+            np.array(
+                [
+                    [None, None, "*", "*"],
+                    ["*", "*", "*", None],
+                ]
+            ),
+        )
+
+
+class fig4(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 4",
+            np.array(
+                [
+                    ["*", None, None],
+                    ["*", "*", None],
+                    [None, "*", "*"],
+                ]
+            ),
+        )
+
+
+class fig5(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 5",
+            np.array(
+                [
+                    ["*", "*", "*", "*", "*"],
+                ]
+            ),
+        )
+
+
+class fig6(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 6",
+            np.array(
+                [
+                    ["*", None, None],
+                    ["*", None, None],
+                    ["*", "*", "*"],
+                ]
+            ),
+        )
+
+
+class fig7(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 7",
+            np.array(
+                [
+                    ["*", "*", "*", "*"],
+                    [None, None, None, "*"],
+                ]
+            ),
+        )
+
+
+class fig8(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 8",
+            np.array(
+                [
+                    [None, None, None, "*"],
+                    ["*", "*", "*", "*"],
+                ]
+            ),
+        )
+
+
+class fig9(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 9",
+            np.array(
+                [
+                    [None, None, "*"],
+                    ["*", "*", "*"],
+                    [None, "*", None],
+                ]
+            ),
+        )
+
+
+class fig10(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 10",
+            np.array(
+                [
+                    [None, None, "*"],
+                    ["*", "*", "*"],
+                    ["*", None, None],
+                ]
+            ),
+        )
+
+
+class fig11(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 11",
+            np.array(
+                [
+                    ["*", None, None],
+                    ["*", "*", "*"],
+                    [None, "*", None],
+                ]
+            ),
+        )
+
+
+class fig12(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 12",
+            np.array(
+                [
+                    ["*", None, None],
+                    ["*", "*", "*"],
+                    [None, None, "*"],
+                ]
+            ),
+        )
+
+
+class fig13(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 13",
+            np.array(
+                [
+                    ["*", "*", "*", "*"],
+                    [None, None, "*", None],
+                ]
+            ),
+        )
+
+
+class fig14(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 14",
+            np.array(
+                [
+                    [None, None, "*", None],
+                    ["*", "*", "*", "*"],
+                ]
+            ),
+        )
+
+
+class fig15(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 15",
+            np.array(
+                [
+                    [None, "*", "*"],
+                    ["*", "*", "*"],
+                ]
+            ),
+        )
+
+
+class fig16(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 16",
+            np.array(
+                [
+                    ["*", None, "*"],
+                    ["*", "*", "*"],
+                ]
+            ),
+        )
+
+
+class fig17(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 17",
+            np.array(
+                [
+                    [None, "*", None],
+                    ["*", "*", "*"],
+                    [None, "*", None],
+                ]
+            ),
+        )
+
+
+class fig18(Figure):
+    def __init__(self):
+        super().__init__(
+            "Figura Difícil 18",
+            np.array(
+                [
+                    ["*", "*", "*"],
+                    [None, "*", "*"],
+                ]
+            ),
+        )
